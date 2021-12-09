de886873acf85ca0061ad812175b1902adabc97d
Pieterjan Spoelders <pj.spoelders@gmail.com>
2021-12-09 16:29:18 +0100

    current wip

--- error-reporting-csharp-dotnet-tool/ErrorCodeCollection.cs
+++ error-reporting-csharp-dotnet-tool/ErrorCodeCollection.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace error_reporting_csharp_dotnet_tool
 {
@@ -8,6 +11,9 @@ namespace error_reporting_csharp_dotnet_tool
     {
         private Dictionary<string, ErrorCodeEntry> errorCodeDictionary;
         public string ProjectShortTag { get; set; }
+
+        public string ProjectName { get; set; }
+        public string ProjectVersion { get; set; }
         public ErrorCodeCollection()
         {
             errorCodeDictionary = new Dictionary<string, ErrorCodeEntry>();
@@ -15,7 +21,8 @@ namespace error_reporting_csharp_dotnet_tool
 
         public ErrorCodeEntry AddEntry(String identifier)
         {
-            if (errorCodeDictionary.ContainsKey(identifier)){
+            if (errorCodeDictionary.ContainsKey(identifier))
+            {
                 throw new Exception($@"Identifier {identifier} previously used in project(s). Please correct this.");
             }
             if (!identifier.Contains(ProjectShortTag))
@@ -27,10 +34,46 @@ namespace error_reporting_csharp_dotnet_tool
             errorCodeDictionary.Add(identifier, newECEntry);
             return newECEntry;
         }
-
+        //https://www.newtonsoft.com/json/help/html/readingwritingjson.htm
         public void BuildJSON()
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+
+                writer.WriteStartObject();
+
+                writer.WritePropertyName("$schema");
+                writer.WriteValue("https://schemas.exasol.com/error_code_report-0.2.0.json");
+
+                writer.WritePropertyName("projectName");
+                writer.WriteValue(ProjectName);
+
+                writer.WritePropertyName("projectVersion");
+                writer.WriteValue(ProjectVersion);
+
+                writer.WritePropertyName("errorCodes");
+                writer.WriteStartArray();
+                //start of error codes array
+                WriteErrorCodeEntry(writer);
+                //end of error codes array
+                writer.WriteEnd();
+
+                writer.WriteEndObject();
+
+            }
+        }
+
+        private static void WriteErrorCodeEntry(JsonWriter writer)
+        {
+            writer.WriteValue("DVD read/writer");
+            writer.WriteComment("(broken)");
+
+            writer.WriteValue("500 gigabyte hard drive");
+            writer.WriteValue("200 gigabyte hard drive");
         }
     }
 }
