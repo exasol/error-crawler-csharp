9d668194a44e0c3b80650c57f5707dfff5aa5ece
Pieterjan Spoelders <pj.spoelders@gmail.com>
2022-01-04 13:32:06 +0100

    fixed old test and added new integration test with no project name specified

--- error-reporting-csharp-dotnet-tool-tests/Tests.cs
+++ error-reporting-csharp-dotnet-tool-tests/Tests.cs
@@ -24,7 +24,15 @@ namespace error_reporting_csharp_dotnet_tool_tests
             RunErrorCrawlerOnTestProject();
             CheckResultingJSON();
         }
-
+        [Fact]
+        public void IntegrationTestNoProjectTag()
+        {
+            File.Delete("error_code_report.json");
+            PackageAndInstallTool();
+            BuildTestProject();
+            RunErrorCrawlerOnTestProjectWithoutProjectName();
+            CheckResultingJSONWithoutProjectName();
+        }
         private static void PackageAndInstallTool()
         {
             string output = RunCmd("cd ../../../.. && dotnet pack");
@@ -68,8 +76,20 @@ namespace error_reporting_csharp_dotnet_tool_tests
         private static void RunErrorCrawlerOnTestProject()
         {
             string projectEntry = "..\\..\\..\\..\\error-reporting-dotnet-tool-testproject\\error-reporting-dotnet-tool-testproject.csproj";
+            string argument = $"exasol-error-crawler -t ECC -p {projectEntry} -n error-crawler-csharp";
+            RunCmd(argument);
+        }
+        private static void RunErrorCrawlerOnTestProjectWithoutProjectName()
+        {
+            string projectEntry = "..\\..\\..\\..\\error-reporting-dotnet-tool-testproject\\error-reporting-dotnet-tool-testproject.csproj";
             string argument = $"exasol-error-crawler -t ECC -p {projectEntry}";
             RunCmd(argument);
+        }
+        private static void CheckResultingJSONWithoutProjectName()
+        {
+            var jsonStr = File.ReadAllText("error_code_report.json");
+            var compareStr = "{\r\n  \"$schema\": \"https://schemas.exasol.com/error_code_report-1.0.0.json\",\r\n  \"errorCodes\": [\r\n    {\r\n      \"identifier\": \"E-ECC-TEST-1\",\r\n      \"message\": \"\",\r\n      \"mitigations\": []\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TEST-2\",\r\n      \"message\": \"\",\r\n      \"mitigations\": []\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TEST-3\",\r\n      \"message\": \"Woops! Something went wrong! 1\",\r\n      \"mitigations\": []\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TEST-4\",\r\n      \"message\": \"Woops! Something went wrong! 1\",\r\n      \"mitigations\": []\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TEST-4bis\",\r\n      \"message\": \"Woops! Something went wrong! 1Oh my! Something went wrong! 2\",\r\n      \"mitigations\": []\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TESTMIT-1\",\r\n      \"message\": \"Woops! Something went wrong!\",\r\n      \"mitigations\": [\r\n        \"Do something about it 1\"\r\n      ]\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TESTMIT-2\",\r\n      \"message\": \"Woops! Something went wrong!\",\r\n      \"mitigations\": [\r\n        \"Do something about it 1\",\r\n        \"Don't just sit there 2\"\r\n      ]\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TESTSI-1\",\r\n      \"message\": \"Woops! {somethingWentWrongValue}\",\r\n      \"mitigations\": []\r\n    },\r\n    {\r\n      \"identifier\": \"E-ECC-TESTCONCAT-1\",\r\n      \"message\": \"Woops! + Woops!\",\r\n      \"mitigations\": []\r\n    }\r\n  ]\r\n}";
+            Assert.True(jsonStr == compareStr);
         }
         //https://stackoverflow.com/questions/4291912/process-start-how-to-get-the-output
         private static string RunCmd(string argument)
